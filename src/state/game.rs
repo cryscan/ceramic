2dc6fa87f32f2aed5a0068bc21eb7f26227bf03d
cryscan <cryscan@umich.edu>
2020-01-22 04:48:56 -0500

    Handle quit inputs.

--- src/state/game.rs
+++ src/state/game.rs
@@ -1,4 +1,7 @@
-use amethyst::prelude::*;
+use amethyst::{
+    input::{get_key, is_close_requested, is_key_down, StringBindings, VirtualKeyCode},
+    prelude::*,
+};
 
 use crate::scene::Scene;
 
@@ -8,5 +11,22 @@ impl SimpleState for GameState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let scene_handle = data.world.fetch::<Scene>().handle.as_ref().unwrap().clone();
         data.world.create_entity().with(scene_handle).build();
+    }
+
+    fn handle_event(
+        &mut self,
+        _data: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent<StringBindings>,
+    ) -> Trans<GameData<'static, 'static>, StateEvent<StringBindings>> {
+        if let StateEvent::Window(event) = &event {
+            if is_close_requested(event) || is_key_down(event, VirtualKeyCode::Escape) {
+                return Trans::Quit;
+            }
+            match get_key(&event) {
+                Some(_) => {}
+                None => {}
+            }
+        }
+        Trans::None
     }
 }
